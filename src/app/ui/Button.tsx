5d599c0542602ce6f499f57fc3dbfc9a2ae7de54
matsusht24 <matsusht24@up.edu>
2024-11-14 22:34:12 -0800

    added svg for buttons on Home Page

--- src/app/ui/Button.tsx
+++ src/app/ui/Button.tsx
@@ -1,18 +1,29 @@
 import Link from 'next/link';
 import React from 'react'
-
+import Image from 'next/image';
 const link_dict = {
-    'Experiences': "/experiences",
-    'Projects': "/projects",
+    'Experiences': {
+        link:"/experiences",
+        img: "/experience.svg",
+        alt: "experience"
+    },
+    'Projects': {
+        link: "/projects",
+        img: "/project.svg",
+        alt: "project"
+    },
 };
 
 type ButtonProps = 'Experiences'|'Projects';
 
 
 function Button({label}: {label: ButtonProps}) {
+    const cur_button = link_dict[label];
   return (
     <div >
-        <Link href={link_dict[label] ?? "/"} className="text-lg font-semibold w-40 h-32 bg-gray-200 flex items-center justify-center rounded-lg shadow-md hover:bg-gray-300 transition-all duration-300">{label}</Link>
+        <Link href={cur_button.link ?? "/"} className="w-40 h-3 flex items-center justify-center rounded-lg shadow-md">
+            <Image width={200} height={100} alt={cur_button.alt} src={cur_button.img} />
+        </Link>
     </div>
     );
 }
