15525111513231cb1114c2013dadf449397d1798
matsusht24 <matsusht24@up.edu>
2024-11-16 15:36:55 -0800

    Created Navigation bar component, Designed project and experience page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
+import NavBar from '../ui/NavBar'
+
+type project = {
+  name: string, 
+  date: string, 
+  skills: [string],
+  img: string,
+  
+};
 
 function page() {
   return (
-    <div>page</div>
+    <div className="wrapper">
+      <div className="header">
+        <NavBar/>
+        
+        </div>
+    </div>
   )
 }
 
---
+++ src/app/ui/NavBar.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import React from 'react'
+
+function NavBar() {
+    const pages = {'Home': "/", 'Projects': "/projects", 'Experiences': "/experiences"};
+  return (
+    <nav>
+        <ul className='flex gap-8 h-auto w-auto'>
+        {Object.entries(pages).map(([name, url]) => (
+            
+            <li key={name} className='font-bold w-auto' >
+                
+                <Link href={url} className='text-xl'>{name}</Link>
+            </li>
+        ))}
+        </ul>
+        
+        
+    </nav>
+    
+  )
+}
+
+export default NavBar
