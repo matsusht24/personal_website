cfd97f80cf42aec41c421ec8ffade821d4a52e7c
matsusht24 <matsusht24@up.edu>
2024-11-22 16:09:35 -0800

    started the interaction w cards

---
+++ src/app/ui/ExpandedCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface cards{
+    name: string,
+    skills: string[],
+    description: string,  
+};
+
+
+
+function ExpandedCard() {
+  return (
+    <div>ExpandedCard</div>
+  )
+}
+
+export default ExpandedCard
