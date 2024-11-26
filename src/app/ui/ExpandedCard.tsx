8bd51a5faeb1f6c9d4a2603edc40da08a946d360
matsusht24 <matsusht24@up.edu>
2024-11-26 12:38:54 -0800

    formatting for expanding card using grid

--- src/app/ui/ExpandedCard.tsx
+++ src/app/ui/ExpandedCard.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import { expType } from '../experiences/page';
 
-interface cardType extends expType{
-    skills: string[],
-    description: string,  
-};
-
-const cards: {[id: string]: cardType;} = {
-  'Alluvion, Inc': {name: "Alluvion, Inc", skills: ['UI Design', 'Wix', 'SEO'], description: "Created a website to advertise and attract orders for florist and nursery needs."},
-  'Genki Spark': {name: "Genki"}
-};
-
 
 function ExpandedCard() {
   return (
-    <div className="w-full h-full border-dotted border-black">
-      
+    <div className="relative w-full h-full border-dotted border-black border-2">
+      <strong>Test</strong>
+      <div className="grid grid-cols-4 gap-4 bg-black ">
+        <div className="bg-white p-10 text-center text-lg row-span-2 ">1</div>
+        <div className="bg-white p-10 text-center text-lg ">2</div>
+        <div className="bg-white p-10 text-center text-lg ">3</div>
+        
+
+      </div>
     </div>
   )
 }
