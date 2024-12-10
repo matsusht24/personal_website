b53b297f5c8b2b98e62e5b01468cf3d1baa4ca6a
matsusht24 <matsusht24@up.edu>
2024-12-09 14:11:01 -1000

    removing dynamic image loading

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/personal_website",
-  assetPrefix: "/personal_website"
+  assetPrefix: "/personal_website",
+  images: {
+    unoptimized: true, //prevents dynamic image loading
+  }
 
 };
 
