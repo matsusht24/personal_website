d33fa4f9b9c3e238e450a5d01b40874bc8e7fdc4
matsusht24 <matsusht24@up.edu>
2024-12-09 14:07:08 -1000

    changed assetPrefix in next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/personal_website"
+  basePath: "/personal_website",
+  assetPrefix: "/personal_website"
 
 };
 
