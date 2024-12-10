ac6d6b7fe6d965ff20d5bef5b43aaec4541b2c0a
matsusht24 <matsusht24@up.edu>
2024-12-09 14:26:18 -1000

    removed basePath and export in next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/personal_website",
-  assetPrefix: "/personal_website",
-  images: {
-    unoptimized: true, //prevents dynamic image loading
-  }
+  //output: "export",
+  //basePath: "/personal_website",
+  
 
 };
 
